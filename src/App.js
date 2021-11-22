a755c3210fe1f9dae7db533d0db92986a4c6d8ab
GW <kowonp@gmail.com>
2021-11-22 15:48:41 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import Button from "./Button";
-import ModalButton from "./ModalButton";
+import { useEffect, useState } from "react";
 import styles from "./App.module.css";
 
 const App = () => {
+  const [counter, setConter] = useState(0);
+
+  const handleClick = () => {
+    setConter((counter) => counter + 1);
+  };
+  console.log("render");
+
+  useEffect(() => {
+    console.log("useEffect");
+  }, []);
+
   return (
     <div>
       <h1 className={styles.title}>App</h1>
-      <Button text="Save" />
-      <Button text="Delete" />
-      <ModalButton text="Modal" />
+      <h3>{counter}</h3>
+      <button onClick={handleClick}>Add</button>
     </div>
   );
 };
