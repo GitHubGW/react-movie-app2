dfc312e2134a721c6ec97898199a03b818b55385
GW <kowonp@gmail.com>
2021-11-23 17:40:33 +0900

    useEffect Cleanup Function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import { useEffect, useState } from "react";
 import styles from "./App.module.css";
 
+const Hello = () => {
+  useEffect(() => {
+    console.log("Start useEffect");
+    return () => console.log("Finish useEffect");
+  }, []);
+
+  return <h1>Hello</h1>;
+};
+
 const App = () => {
   const [counter, setConter] = useState(0);
   const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
 
   const handleClick = () => {
     setConter((counter) => counter + 1);
@@ -15,6 +25,10 @@ const App = () => {
 
   const handleFormClick = (event) => {
     event.preventDefault();
+  };
+
+  const handleShowing = () => {
+    setShowing((showing) => !showing);
   };
 
   useEffect(() => {
@@ -42,11 +56,18 @@ const App = () => {
       </div>
 
       <form>
+        <h1>Keyword</h1>
         <input type="text" placeholder="Search keyword" value={keyword} onChange={handleInputChange} />
         <button type="submit" onClick={handleFormClick}>
           검색
         </button>
       </form>
+
+      <div>
+        <h1>Showing</h1>
+        <button onClick={handleShowing}>{showing ? "Hide" : "Show"}</button>
+        {showing && <Hello />}
+      </div>
     </div>
   );
 };
