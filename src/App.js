0432447b1bec9f091607d6e7d23f7e480073a508
GW <kowonp@gmail.com>
2021-11-25 16:05:08 +0900

    Completed Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((toDos) => [...toDos, toDo]);
-    setToDo("");
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [prices, setPrices] = useState("");
+  const [coinData, setCoinData] = useState(0);
 
   const handleChange = (event) => {
-    setToDo(event.target.value);
+    setPrices(event.target.value);
   };
+
+  const handleChangeSelect = (event) => {
+    setCoinData(event.target.value);
+  };
+
+  useEffect(() => {
+    const handleFetch = async () => {
+      fetch("https://api.coinpaprika.com/v1/tickers")
+        .then((response) => response.json())
+        .then((json) => {
+          setCoins(json);
+          setLoading(false);
+        });
+    };
+    handleFetch();
+  }, []);
 
   return (
     <div>
-      <h1>To Do ({toDos.length})</h1>
-      <form onSubmit={handleSubmit}>
-        <input value={toDo} onChange={handleChange} type="text" placeholder="Write to do" />
-        <button type="submit">Add</button>
-      </form>
-      <ul>
-        {toDos.map((todo, index) => (
-          <li key={index}>{todo}</li>
-        ))}
-      </ul>
+      <h1>Coin Tracker ({coins.length})</h1>
+      {loading ? (
+        <h1>Loading...⚛️</h1>
+      ) : (
+        <div>
+          <h2>KRW 👉 Coin</h2>
+          <input value={prices} onChange={handleChange} type="number" placeholder="금액을 입력해주세요."></input>원
+          <br />
+          <select onChange={handleChangeSelect}>
+            <option>Select Coin</option>
+            {coins.map((coin) => (
+              <option key={coin.id} value={coin.quotes.USD.price * 1000}>
+                #{coin.rank}: {coin.name} / {coin.symbol} / ${coin.quotes.USD.price.toFixed(1)}
+              </option>
+            ))}
+          </select>
+          <h3>환전한 코인: {prices !== 0 && coinData !== 0 && prices / coinData}</h3>
+        </div>
+      )}
     </div>
   );
 };
